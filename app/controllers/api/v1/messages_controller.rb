a8dcbed675d47d844769050963fcdde4d63cf7c0
TorreJohnson <torre.johnson@gmail.com>
2018-04-10 18:51:37 -0400

    adjusted routes for messages controller

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    byebug
     if @message.save
       render json: @message
     else
