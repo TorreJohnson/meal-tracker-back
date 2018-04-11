ac9234d488124d92bd4cda75c69677ab6c039f62
TorreJohnson <torre.johnson@gmail.com>
2018-04-11 13:25:15 -0400

    updated controller for nutritionists

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::AuthController < ApplicationController
       render json: {user: @user, food_items: @user.food_items, messages: @user.messages, jwt: jwt}
     elsif @nutritionist && @nutritionist.authenticate(params[:password])
       jwt = issue_token({nutritionist_id: @nutritionist.id})
-      render json: {user: @nutritionist, jwt: jwt}
+      render json: {user: @nutritionist, messages: @nutritionist.messages, jwt: jwt}
     else
       render json: {error: "Invalid Username and/or Password"}
     end
